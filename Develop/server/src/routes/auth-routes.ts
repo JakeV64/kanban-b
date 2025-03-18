6517985a4b6dbd15c3453499f3ea7a32510d3d9c
JakeV64 <jakevonah@gmail.com>
2025-03-17 20:51:42 -0500

    authentication token

--- Develop/server/src/routes/auth-routes.ts
+++ Develop/server/src/routes/auth-routes.ts
@@ -3,8 +3,22 @@ import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
-export const login = async (req: Request, res: Response) => {
+export const login = async (req: Request, res: Response) => { 
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ where: { username } });
+
+  if (!user) { 
+    return res.status(404).json({ message: 'User not found' });
+  }
+  const passwordMatch = await bcrypt.compare(password, user.password);
+   if (!passwordMatch) {
+    return res.status(401).json({ message: 'Invalid password' });
+  } 
+
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+  return res.status(200).json({ token });
 };
 
 const router = Router();
